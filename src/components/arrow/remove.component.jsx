f114ec91c65f27c7e3820736dd978b8bb4befe11
bitz161 <bitzgarcia1996@gmail.com>
2022-07-21 18:57:28 +0800

    count cart

---
+++ src/components/arrow/remove.component.jsx
@@ -0,0 +1,7 @@
+import { ReactComponent as RemoveIcon } from "../../assets/remove.svg";
+
+const Remove = () => {
+  return <RemoveIcon />;
+};
+
+export default Remove;
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,11 @@
 import Button from "../button/button.component";
 import "./cart-dropdown.styles.scss";
+import { Routes, Route } from "react-router-dom";
 import CartItem from "../cart-item/cart-item.component";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 import { MessageContext } from "../../contexts/empty-message.context";
+import { Link } from "react-router-dom";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
@@ -16,7 +18,10 @@ const CartDropdown = () => {
           <CartItem cartItem={item} key={item.id} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+
+      <Link to="cart">
+        <Button>GO TO CHECKOUT</Button>
+      </Link>
     </div>
   );
 };
