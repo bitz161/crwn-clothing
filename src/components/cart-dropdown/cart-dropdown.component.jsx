67f0d15bfdf7e66622720503f7271de7df02bfc9
bitz161 <68050804+bitz161@users.noreply.github.com>
2022-07-22 14:53:51 +0800

    Revert "count cart"

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,9 @@
 import Button from "../button/button.component";
 import "./cart-dropdown.styles.scss";
-import { Routes, Route } from "react-router-dom";
 import CartItem from "../cart-item/cart-item.component";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 import { MessageContext } from "../../contexts/empty-message.context";
-import { Link } from "react-router-dom";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
@@ -18,10 +16,7 @@ const CartDropdown = () => {
           <CartItem cartItem={item} key={item.id} />
         ))}
       </div>
-
-      <Link to="cart">
-        <Button>GO TO CHECKOUT</Button>
-      </Link>
+      <Button>GO TO CHECKOUT</Button>
     </div>
   );
 };
