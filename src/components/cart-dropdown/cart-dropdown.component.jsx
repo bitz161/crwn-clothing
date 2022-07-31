4fe2013f6649281a1e555d8d9cfa36bd86357b3c
bitz161 <bitzgarcia1996@gmail.com>
2022-07-31 22:15:40 +0800

    update

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,10 +4,7 @@ import CartItem from "../cart-item/cart-item.component";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 import { MessageContext } from "../../contexts/empty-message.context";
-<<<<<<< HEAD
 import { useNavigate } from "react-router-dom";
-=======
->>>>>>> ec5d808bbb4ca549969088cf00f1d5d8a34d3f83
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
@@ -27,11 +24,8 @@ const CartDropdown = () => {
           <CartItem cartItem={item} key={item.id} />
         ))}
       </div>
-<<<<<<< HEAD
       <Button onClick={goToCheckOutHandler}>GO TO CHECKOUT</Button>
-=======
       <Button>GO TO CHECKOUT</Button>
->>>>>>> ec5d808bbb4ca549969088cf00f1d5d8a34d3f83
     </div>
   );
 };
